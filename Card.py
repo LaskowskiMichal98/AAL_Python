3a39421e06679860737ede331f5bcf743b73cd11
Paweł Lech <pawellechtoja@gmail.com>
2021-01-18 20:36:35 +0100

    nowe funkcje Card

--- Card.py
+++ Card.py
@@ -1,9 +1,32 @@
+import math
+
 
 class Card:
-
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = round(x, 3)
+        self.y = round(y, 3)
+
+    def alpha(self):
+        return round(math.atan(self.y / self.x)*180/math.pi, 3)  # angle between OX and vector in degrees
+
+    def rotate(self, angle):
+        angle_rad = angle/180*math.pi
+        new_x = self.x * math.cos(angle_rad) - self.y * math.sin(angle_rad)
+        new_y = self.x * math.sin(angle_rad) + self.y * math.cos(angle_rad)
+        return Card(new_x, new_y)
+
+    def normalize(self):
+        return Card(self.x/len(self), self.y/len(self))
+
+    def __len__(self):
+        return round(math.sqrt(self.x**2 + self.y**2), 3)
 
     def __str__(self):
         return f'X: {self.x}, Y: {self.y}'
+
+    def __eq__(self, other):
+        return self.alpha() == other.alpha()
+
+    def __lt__(self, other):
+        return self.alpha() < other.alpha()
+
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import Generator
 import Data
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     Generator.generate(100, "test.txt")
     data = Data.Data()
@@ -20,4 +9,3 @@ if __name__ == '__main__':
     for i in range(len(data.list_of_cards)):
         print(data.list_of_cards[i].__str__())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
