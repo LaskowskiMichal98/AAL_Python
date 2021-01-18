f0c63de5409830f7e18f1a2196ad778e0b9b5221
Michał Laskowski <m.laskowski@stud.elka.pw.edu.pl>
2021-01-18 17:05:41 +0100

    Initial commit

---
+++ Card.py
@@ -0,0 +1,9 @@
+
+class Card:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'X: {self.x}, Y: {self.y}'
---
+++ Data.py
@@ -0,0 +1,12 @@
+
+class Data:
+
+    def __init__(self):
+        self.list_of_cars = list()
+
+    def add_card(self, card):
+        self.list_of_cars.append(card)
+
+
+def read_from_file(path):
+    
